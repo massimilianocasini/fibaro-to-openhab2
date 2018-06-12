7750e457791b524cbede745048b1f681b2277b21
massimilianocasini <massimiliano.casini@gmail.com>
2018-06-12 23:57:00 +0200

    Update

--- amanda/createimage.sh
+++ amanda/createimage.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 now="$(date +'%Y%m%d%H%M%S')"
 host=$(hostname)
+ver="$(dpkg -s openhab2|grep Version|cut -c10-16)"
+find /media/usb/images/ -type f -mtime +1 -delete
+/usr/sbin/amfetchdump -paC ohasis-bkp ohasis /dev/mmcblk0 > /media/usb/images/"$now"-"$host"-"$ver"-image.raw
 
-/usr/sbin/amfetchdump -pa ohasis-bkp ohasis /dev/mmcblk0 > /media/usb/images/"$now"-"$host"-image.raw
-find /media/usb/images/ -type f -mtime +2 -delete
